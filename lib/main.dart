3a73be7714f80e7de3caa0112caa87f2ff58a258
Jeff <jeff.neet@gmail.com>
2020-10-03 12:00:53 -0600

    Customize to pull Image from Gravatar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,91 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 120.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage(
+                    'https://www.gravatar.com/avatar/1f041416604b0a30bf950a4ff06b0dab?s=800'),
+              ),
+              Text(
+                'Jeff Neet',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'PRODUCER  DESIGNER  ENGINEER',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 160,
+                height: 30,
+                child: Divider(
+                  color: Colors.tealAccent,
+                ),
+              ),
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.tealAccent.shade700,
+                    ),
+                    title: Text(
+                      '+1 970 381 9899',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                      ),
+                    ),
+                  )),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.tealAccent.shade700,
+                  ),
+                  title: Text(
+                    'jeff.neet@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
